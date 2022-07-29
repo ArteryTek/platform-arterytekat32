7ef381eadd26ffa677b994cbcf759d2e10e55a55
Amo Xu <amo@git.ltd>
2022-07-29 19:35:22 +0800

    rename platform package

--- platform.py
+++ platform.py
@@ -20,7 +20,7 @@ from platform import system
 from platformio.managers.platform import PlatformBase
 from platformio.util import get_systype
 
-class At32Platform(PlatformBase):
+class Arterytekat32Platform(PlatformBase):
 
     def configure_default_packages(self, variables, targets):
         board = variables.get("board")
